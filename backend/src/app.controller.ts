502d3cb2048b52aafed66d26fdc29cd99c0348f5
Danilove <dansousa09@gmail.com>
2024-08-07 11:44:35 -0300

    refactor: Update homepage message in AppController

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
-
 	@Get()
 	getHello(): string {
-		return this.appService.getHello();
+		return `
+			<div style="text-align: center; padding: 1rem;">
+				<h1>Cloud System Backup API is running...</h1>
+			</div>
+		`;
 	}
 }
