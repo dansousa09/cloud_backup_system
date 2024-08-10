3513f6ff25430c5e392763f55271124cbdde9335
aryelzx <arielcearamor102@gmail.com>
2024-08-10 18:33:29 -0300

    feat: install and configure native-base

--- app/src/shared/configs/stack-navigation/index.tsx
+++ app/src/shared/configs/stack-navigation/index.tsx
@@ -1,22 +1,25 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import Routes from "../../../routes/main.routes"
+import Routes from "../../../routes/main.routes";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { AuthContextProvider } from "../../context/auth-context";
+import { NativeBaseProvider } from "native-base";
 
 export const StackNavigator = () => {
   return (
     <NavigationContainer>
-        <SafeAreaView
-          style={{
-            flex: 1,
-            backgroundColor: "#FFFF",
-          }}
-        >
+      <SafeAreaView
+        style={{
+          flex: 1,
+          backgroundColor: "#FFFF",
+        }}
+      >
+        <NativeBaseProvider>
           <AuthContextProvider>
             <Routes />
           </AuthContextProvider>
-        </SafeAreaView>
+        </NativeBaseProvider>
+      </SafeAreaView>
     </NavigationContainer>
   );
 };
