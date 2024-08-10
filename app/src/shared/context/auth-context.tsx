0bf55e58fbe53ad8ed83616fd2d6f06079ebf444
aryelzx <arielcearamor102@gmail.com>
2024-08-10 17:45:09 -0300

    feat: auth context

---
+++ app/src/shared/context/auth-context.tsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState } from 'react';
+import { AuthStateInterface } from './interfaces/auth.interface';
+
+type AuthContextProps = {
+	authState?: {
+		value: AuthStateInterface;
+		setValue: (value: AuthStateInterface) => void;
+	};
+	onLogout?: () => Promise<any>;
+};
+
+const AuthContext = createContext<AuthContextProps>({} as AuthContextProps);
+
+const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+	const [authState, setAuthState] = useState<AuthStateInterface>({
+		authenticated: false,
+		token: null,
+	});
+
+	const value: AuthContextProps = {
+		authState: {
+			value: authState,
+			setValue: setAuthState,
+		},
+	};
+
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+const useAuthContext = () => {
+	return useContext(AuthContext);
+};
+export { AuthContextProvider, useAuthContext };
---
+++ app/src/shared/context/interfaces/auth.interface.ts
@@ -0,0 +1,4 @@
+export interface AuthStateInterface {
+	token: string | null;
+	authenticated: boolean | null
+}
